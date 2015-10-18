73f34fdc7c68de4e68642666abded68f65b9179a
martinosorb <martino.sorbaro@ed.ac.uk>
2015-10-18 16:41:52 -0500

    added benchmark file

---
+++ benchmark.py
@@ -0,0 +1,15 @@
+from ising_sampling import IsingModel
+import numpy as np
+
+numspin = 20
+n = 1000
+
+np.random.seed(56426481)
+h = np.random.normal(size=numspin)
+j = np.random.normal(size=(numspin, numspin))
+j += j.T
+j[np.diag_indices_from(j)] = np.zeros(numspin)
+np.random.seed()
+
+model = IsingModel(h, j)
+model.sample(n)
