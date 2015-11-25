5c21e8f7a4c319946fb13583e7c6711950a7151a
martinosorb <martino.sorbaro@ed.ac.uk>
2015-11-25 15:30:29 +0000

    updated test to new system, from cython version

--- test.py
+++ test.py
@@ -7,6 +7,13 @@ import numpy as np
 # at local fields h and interactions J
 
 
+def from_shaped_iter(iterable, dtype, shape):
+    a = np.empty(shape, dtype)
+    for i, x in enumerate(iterable):
+        a[i] = x
+    return a
+
+
 class TestIsingModel(unittest.TestCase):
 
     def test_input_data(self):
@@ -14,17 +21,17 @@ class TestIsingModel(unittest.TestCase):
             model = IsingModel([0, 0, 0], [[0, 0], [0, 0]])
         with self.assertRaises(ValueError):
             model = IsingModel([0, 0], [[0, 0], [0, 0]])
-            model.sample(-1)
+            list(model.sample(-1))
 
     def test_res_shapes(self):
         model = IsingModel([0, 0], [[0, 0], [0, 0]])
-        res = model.sample(3)
+        res = from_shaped_iter(model.sample(3), bool, (3, 2))
         self.assertEqual(len(res[0]), 2)
         self.assertEqual(len(res[:, 0]), 3)
 
     def test_shape_null(self):
         model = IsingModel([0], [[0]])
-        res = model.sample(1)
+        res = from_shaped_iter(model.sample(1), bool, (1, 1))
         self.assertEqual(res.shape[0], 1)
         self.assertEqual(res.shape[1], 1)
 
@@ -32,7 +39,7 @@ class TestIsingModel(unittest.TestCase):
         h = [np.random.normal()]
         j_matrix = [[0]]
         model = IsingModel(h, j_matrix)
-        res = model.sample(20000)
+        res = from_shaped_iter(model.sample(20000), bool, (20000, 1))
         p1 = 1 / (1 + np.exp(-h[0]))
         obs_prob = np.mean(res, axis=0)[0]
         self.assertAlmostEqual(p1, obs_prob, places=2)
@@ -42,7 +49,7 @@ class TestIsingModel(unittest.TestCase):
         j = np.random.normal()
         j_matrix = [[0, j], [j, 0]]
         model = IsingModel(h, j_matrix)
-        res = model.sample(20000)
+        res = from_shaped_iter(model.sample(20000), bool, (20000, 2))
         prod = res[:, 0] * res[:, 1]
         p1 = np.mean(prod)
         self.assertAlmostEqual(p1, 1 / (3 * np.exp(-j) + 1), places=2)
@@ -52,10 +59,25 @@ class TestIsingModel(unittest.TestCase):
         j = np.random.normal()
         j_matrix = [[0, j], [j, 0]]
         model = IsingModel(h, j_matrix)
-        self.assertAlmostEqual(model.hamiltonian([False, False]), 0)
-        model.h = np.array([2, 2, 2])
-        model.j = np.array([[0, 1, -2], [1, 0, -1], [-2, -1, 0]])
-        self.assertAlmostEqual(model.hamiltonian([True, False, True]), -2.)
+        self.assertAlmostEqual(model.hamiltonian(np.array([0, 0])), 0)
+        h = np.array([-1, 1, -1])
+        j_matrix = np.array([[0, 1, 2], [1, 0, -1], [2, -1, 0]])
+        s = np.array([1, 1, 0])
+        a = np.dot(h, s) + .5 * np.dot(s, np.dot(j_matrix, s))
+        model = IsingModel(h, j_matrix)
+        self.assertAlmostEqual(model.hamiltonian(s), -a)
+
+    def test_submodel(self):
+        j = np.random.normal(0, 1, size=(10, 10))
+        j += j.T
+        j /= 2.
+        j[np.diag_indices_from(j)] = np.zeros(10)
+        h = np.random.random(size=10)
+        model = IsingModel(h, j)
+        subm = model.submodel(5)
+        h10 = model.hamiltonian(np.array([1, 1, 1, 1, 1, 0, 0, 0, 0, 0]))
+        h5 = subm.hamiltonian(np.array([1, 1, 1, 1, 1]))
+        self.assertAlmostEqual(h5, h10)
 
 
 if __name__ == '__main__':
