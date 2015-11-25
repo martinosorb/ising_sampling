9f6b7d3f363f3b6e9cda4f50976e4127e947a771
martinosorb <martino.sorbaro@ed.ac.uk>
2015-11-25 16:56:20 +0000

    changed tests and added mf hamiltonian

--- ising_sampling.py
+++ ising_sampling.py
@@ -3,27 +3,36 @@ import numpy as np
 
 class IsingModel():
     """A class to sample from arbitrary Ising models.
-    The constructor takes the local fields h and connection matrix j.
-    The number of units is inferred from their sizes."""
-    def __init__(self, h, j):
+    The constructor takes the number of spins n, local fields h
+    and connection matrix j."""
+    def __init__(self, n, h, j):
         h = np.asarray(h)
         j = np.asarray(j)
         # size checks
-        numspin = len(h)
-        if j.shape != (numspin, numspin):
-            raise ValueError('Inconsistent h and J sizes')
+        if type(n) is not int or n <= 0:
+            raise ValueError('n must be a positive integer')
+        self.numspin = n
+        if np.size(h) == 1:
+            if np.size(j) != 1:
+                raise ValueError('Inconsistent h and j sizes')
+        else:
+            if len(h) != n or j.shape != (n, n):
+                raise ValueError('Inconsistent h and j sizes')
         if not np.all(j == j.T):
             raise UserWarning('J is not a symmetric matrix')
         # record the values
         self.j = j
         self.h = h
-        self.numspin = numspin
 
     def hamiltonian(self, state=None):
         """The Ising hamiltonian based on the given h and j."""
         if state is None:
             state = self.spins
         state = np.asarray(state)
+
+        if np.size(self.h) == 1:  # mf mode
+            act = np.sum(state)
+            return -self.h * act - 0.5 * self.j * act * (act - 1)
         return - np.dot(self.h, state) - 0.5 * np.dot(state,
                                                       np.dot(self.j, state))
 
@@ -50,4 +59,4 @@ class IsingModel():
     def submodel(self, num):
         h = self.h[:num]
         j = self.j[:num, :num]
-        return IsingModel(h, j)
+        return IsingModel(num, h, j)
