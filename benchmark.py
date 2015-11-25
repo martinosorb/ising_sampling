4bbfe050f02e82be3087a2e23b0e78729dd7826b
martinosorb <martino.sorbaro@ed.ac.uk>
2015-11-25 15:19:36 +0000

    changed benchm par

--- benchmark.py
+++ benchmark.py
@@ -3,8 +3,8 @@ pyximport.install()
 from ising_sampling import IsingModel
 import numpy as np
 
-numspin = 20
-n = 1000
+numspin = 30
+n = 4000
 
 np.random.seed(56426481)
 h = np.random.normal(size=numspin)
